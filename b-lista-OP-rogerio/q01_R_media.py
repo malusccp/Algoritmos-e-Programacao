95de9964e8cbd9285737f8d709bcc0154a2df3ea
Maria Luiza Morais <luizamorais2007@gmail.com>
2025-05-11 17:04:14 -0300

    Add files via upload

---
+++ b-lista-OP-rogerio/q01_R_media.py
@@ -0,0 +1,10 @@
+
+# Entrada 
+numero1 = int(input('Insira o número: '))
+numero2 = int(input('Insira o número: '))
+numero3 = int(input('Insira o número: '))
+# Processamento 
+media_simples = (numero1 + numero2+ numero3) / 3
+
+# Saída
+print(f'>>>> Portanto a media dos números {media_simples}')
