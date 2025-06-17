01871bd23196607d70d6377423f07d694caffd2f
Maria Luiza <luizamorais2007@gmail.com>
2025-06-17 09:42:15 -0300

    Exercícios

--- s-wordplay/utils.py
+++ s-wordplay/utils.py
@@ -1,11 +1,45 @@
-def have_char(word, char):
+import os
+
+def have_char(word, caractere):
     for char in word:
-        if char == word:
+        if char == caractere:
             return True
         
     return False
-        
 
+def uses_only(palavra, letra_permitida):
+    for letra in palavra:
+        if not have_char(letra_permitida, letra):
+            return False
+    return True     
+
+def uses_all(palavra, letras_obrigatorias):
+    for letra in letras_obrigatorias:
+        if not have_char(palavra, letra):
+            return False
+    return True
+
+def clear_screen():
+    os.system('cls')
+
+def is_abecedarian(word):
+  anterior = word[0]
+
+  for i in range(1, len(word)):
+    atual = word[i]
+    if anterior > atual:
+      return False
+    
+    anterior = atual
+  
+  return True
+
+
+def has_no_x(word, caractere_proibido):
+    for caractere in word:
+        if caractere_proibido == caractere:
+            return False
+    return True
 
 def avoids(word, letter_forbidden):
     for char in word:
@@ -13,3 +47,45 @@ def avoids(word, letter_forbidden):
             return False
         
     return True
+
+
+def to_lower(text):
+  new_text = ''
+  for char in text:
+    if is_upper_letter(char):
+      code = ord(char)
+      new_code = code + 32
+      lower_char = chr(new_code)
+      new_text += lower_char
+    else:
+      new_text += char
+
+  return new_text
+
+
+def to_upper(text):
+  new_text = ''
+
+  for letter in text:
+    if is_lower_letter(letter):
+      new_text = new_text + chr(ord(letter) - 32)
+    else:
+      new_text = new_text + letter
+  
+  return new_text
+
+
+def is_upper_letter(char):
+  return ord(char) >= 65 and ord(char) <= 90
+
+
+def is_lower_letter(char):
+  return ord(char) >= 97 and ord(char) <= 122
+
+
+def is_letter(char):
+  return is_lower_letter(char) or is_upper_letter(char)
+
+
+def is_number(char):
+  return ord(char) >= 48 and ord(char) <= 57
--- s-wordplay/word_play.py
+++ s-wordplay/word_play.py
@@ -1,64 +1,126 @@
 import utils
 
 def main():
-    while True:
         menu_word() 
         choose = int(input('Choose a option: '))
-        if choose == no_letter: 
-            has_no_letter()
-        elif choose == size_of_word:
-            size_word()
-        elif choose == letter_forbidden:
-            forbidden = input('Whats the forbidden letter?:')
-            show_word_without_forbbiden_letter(forbidden)
-        elif choose == exit: 
-            break
+        while choose != 0:
+            if choose == size_of_word: 
+                tamanhos_palavra()
+            elif choose == char_proibido:
+                caractere_proibido()
+            elif choose == letra_proibida:
+                letras_proibidas = input('Digite as letras proibidas: ')
+                mostrar_palavra_sem_letras_proibidas(letras_proibidas)
+            elif choose == letra_permitida:
+                letras_permitidas = input('Digite as letras permitidas: ')
+                mostrar_palavra_com_letras_permitidas(letras_permitidas)
+            elif choose == letra_obrigatoria:
+                letras_obrigatorias = input('Digite as letras obrigatórias: ')
+                mostrar_palavra_com_letras_obrigatorias(letras_obrigatorias)
+            elif choose == ordem_alfabetica:
+                mostrar_palavra_em_ordem_alfabetica()
 
-def has_no_letter():
-    letters = str(input('Choose any letter: '))
-    words = open('words.txt')
-    all = 0
-    find = 0
+            input('<Enter to continue>')
+            utils.clear_screen
+            choose = int(input('Choose a option: '))
 
-    for letter in words:
-        all += 1
-        if not letters in letter:
-            word = letter.strip()
-            print(word)
-            find +=1
-    print(f'The percentage of words that has no letter "{letters}" is: {(find/all)*100:.1f}% ')
-
-def size_word():
-    size = int(input('What size do you want?: '))
-    words = open('words.txt')
-    all = 0
-    find = 0
-
-    for letter in words:
-        all += 1
-        if size <= len(letter):
-            word = letter.strip()
-            print(word)
-            find +=1
-    print(f'The percentage of words greater than "{size}" is: {(find/all)*100:.1f}% ')
-
-def show_word_without_forbbiden_letter(forbidden):
-    file = open('br-sem-acentos.txt')
-
-    for line in file:
-        word = line.strip()
-        if utils.avoids(word, forbidden):
-            print(word)
+        
 
 
+
+def tamanhos_palavra():
+    tamanho = int(input('Qual é o tamanho desejado? '))
+    file = open("br-sem-acentos.txt")
+    contador = 0
+    contador_palavras = 0
+    for linha in file:
+        contador += 1
+        palavra = linha.strip()
+        if len(palavra) >= tamanho:
+            print(palavra)
+            contador_palavras += 1
+    porcentagem = (contador_palavras / contador) * 100
+    print(f'A porcentagem de palavras é: {porcentagem:.3f}%')
+
+
+def caractere_proibido():
+    file = open("br-sem-acentos.txt")
+    caractere = input('Caractere proibido: ')
+    contador = 0
+    contador_palavras = 0
+    for linha in file:
+        contador += 1
+        palavra = linha.strip()
+        if utils.has_no_x(palavra, caractere):
+            contador_palavras += 1
+            print(palavra)
+    porcentagem = (contador_palavras / contador) * 100
+    print(f'A porcentagem de palavras é: {porcentagem:.3f}%')
+
+
+def mostrar_palavra_sem_letras_proibidas(letras_proibidas):
+    file = open("br-sem-acentos.txt")
+    contador = 0
+    contador_palavras = 0
+    for linha in file:
+        contador += 1
+        palavra = linha.strip()
+        if utils.avoids(palavra, letras_proibidas):
+            print(palavra)
+            contador_palavras += 1
+    porcentagem = (contador_palavras / contador) * 100
+    print(f'A porcentagem de palavras é: {porcentagem:.3f}%')
+
+
+def mostrar_palavra_com_letras_permitidas(letras_permitidas):
+    file = open("br-sem-acentos.txt")
+    contador = 0
+    contador_palavras = 0
+
+    for linha in file:
+        contador += 1
+        palavra = linha.strip()
+        if utils.uses_only(palavra, letras_permitidas):
+            print(palavra)
+            contador_palavras +=1
+    porcentagem = (contador_palavras / contador) * 100
+    print(f'A porcentagem de palavras é: {porcentagem:.3f}%')
+
+def mostrar_palavra_com_letras_obrigatorias(letras_obrigatorias):
+    file = open("br-sem-acentos.txt")
+    contador = 0
+    contador_palavras = 0
+
+    for linha in file:
+        contador += 1
+        palavra = linha.strip()
+        if utils.uses_all(palavra, letras_obrigatorias):
+            print(palavra)
+            contador_palavras += 1
+    porcentagem = (contador_palavras / contador) * 100
+    print(f'A porcentagem de palavras é: {porcentagem:.3f}%')           
+
+def mostrar_palavra_em_ordem_alfabetica():
+    file = open("br-sem-acentos.txt")
+    contador = 0
+    contador_palavras = 0
+    for linha in file:
+        contador += 1
+        palavra = utils.to_lower(linha.strip())
+        if utils.is_abecedarian(palavra):
+            print(palavra)
+            contador += 1
 
 
 def menu_word():
     interface = '''
 >>> Words Play <<< 
-1 - If you want to choose a letter
-2 - If you want to choose the size of the word
-3 - 
+1 - Palavras com N+ letras
+2 - Palavras sem caractere proibido
+3 - Palavras sem letras proibidas
+4- Palavra com letras permitidas
+5- Palavra com letras obrigatórias
+6- Palavras em ordem alfabética
 
 
 0 - Exit
@@ -66,9 +128,12 @@ def menu_word():
     print(interface)
 
 # Constantes
-no_letter = 1
-size_of_word = 2
-letter_forbidden = 3
+size_of_word = 1
+char_proibido = 2
+letra_proibida = 3
+letra_permitida = 4
+letra_obrigatoria = 5
+ordem_alfabetica = 6
 exit = 0
 
 main()
